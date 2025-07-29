3af68818eb8eb0d3253c89b6a533847b1430b7d2
Muhamad Zain <zainbizniez21@gmail.com>
2025-07-29 15:11:19 +0700

    update rsvp

--- app/rsvp/[id]/page.js
+++ app/rsvp/[id]/page.js
@@ -29,31 +29,31 @@ export default function Rsvp() {
         getData();
     }, [id]);
     return(
-        <section className=" max-w-[400px] bg-slate-700 min-h-screen m-auto">
+        <section className=" max-w-[400px] min-h-screen m-auto">
             <div className="p-5">
                 <h2 className="playfair text-5xl font-bold underline text-center italic animate-pulse py-5">RSVP</h2>
                 <div>
                     <h3 className="py-5">Konfirmasi Kehadiran </h3>
-                    <div className="overflow-x-auto rounded-lg border border-slate-200">
+                    <div className="overflow-x-auto rounded-lg border border-orange-400">
                     <table className=" min-w-full  text-sm leading-normal">
                         <thead className="uppercase">
-                            <tr>
-                            <th className="py-3 px-1 border-b text-center">No</th>
-                            <th className="py-3 px-1 text-left border-b">Nama</th>
-                            <th className="py-3 px-1 text-left border-b">Kehadiran</th>
-                            <th className="py-3 px-1 text-left border-b">Jumlah</th>
+                            <tr className="">
+                            <th className="py-3 px-1 border-b border-orange-400 text-center">No</th>
+                            <th className="py-3 px-1 text-left border-b border-orange-400">Nama</th>
+                            <th className="py-3 px-1 text-left border-b border-orange-400">Kehadiran</th>
+                            <th className="py-3 px-1 text-left border-b border-orange-400">Jumlah</th>
                             </tr>
                         </thead>
                         <tbody>
                         {isLoading ? (
                                     <tr>
-                                        <td colSpan="4" className="text-center py-3 px-6 text-gray-500">
+                                        <td colSpan="4" className="text-center py-3 px-6 text-orange-400">
                                             Data sedang dimuat...
                                         </td>
                                     </tr>
                                 ) : data.length > 0 ? (
                                     data.map((rs, index) => (
-                                        <tr key={rs.id || index} className="border-b ">
+                                        <tr key={rs.id || index} className="border-b border-orange-400 ">
                                             <td className="text-center py-3 px-1">{index + 1}</td>
                                             <td className="py-3 px-1">{rs.name}</td>
                                             <td className="py-3 px-1">{rs.confirm}</td>
@@ -62,7 +62,7 @@ export default function Rsvp() {
                                     ))
                                 ) : (
                                     <tr>
-                                        <td colSpan="4" className="text-center py-3 px-6 text-gray-500">
+                                        <td colSpan="4" className="text-center py-3 px-6 text-orange-400">
                                             Tidak ada data
                                         </td>
                                     </tr>
